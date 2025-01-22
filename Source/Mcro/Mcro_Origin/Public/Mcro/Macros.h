60d7945358436723e6440c0361b4396461c653eb
David Morasz <m@mcro.de>
2025-01-22 22:49:42 +0100

    + MACRO_OVERLOAD helper

--- Source/Mcro/Mcro_Origin/Public/Mcro/Macros.h
+++ Source/Mcro/Mcro_Origin/Public/Mcro/Macros.h
@@ -15,3 +15,23 @@
 #include "HAL/PreprocessorHelpers.h"
 
 #define PREPROCESSOR_TO_TEXT(x) TEXT(PREPROCESSOR_TO_STRING(x))
+
+/**
+ * @brief
+ * Implement preprocessor function overloading based on argument count for a set of macros following a distinct
+ * convention. Individual overloads must have a trailing number corresponding to the number of arguments they accept
+ *
+ * For example:
+ * @code
+ * #define FOO_3(a, b, c) a##b##c
+ * #define FOO_2(a, b)    a##b
+ * #define FOO_1(a)       a
+ *
+ * #define FOO(...) MACRO_OVERLOAD(FOO_, __VA_ARGS__)
+ *
+ * FOO(1) // -> 1
+ * FOO(1, 2) // -> 12
+ * FOO(1, 2, 3) // -> 123
+ * @endcode
+ */
+#define MACRO_OVERLOAD(prefix, ...) PREPROCESSOR_APPEND_VA_ARG_COUNT(prefix, __VA_ARGS__)(__VA_ARGS__)
