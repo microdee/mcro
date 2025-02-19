45c7b4f957a444c5a7320affc5e63fd7c8be3759
David Morasz <m@mcro.de>
2025-02-19 03:15:45 +0100

    use unified tuple utilities

--- Source/Mcro/Mcro_Origin/Public/Mcro/Text/TupleAsString.h
+++ Source/Mcro/Mcro_Origin/Public/Mcro/Text/TupleAsString.h
@@ -18,59 +18,22 @@
 namespace Mcro::Text
 {
 	using namespace Mcro::Tuples;
-	
-	/** @brief Convert Unreal tuples to string the following way: `(Item0, Item1, Item2, ...)`*/
-	template <typename Operand>
-	requires TIsTuple_V<Operand>
-	struct TAsFormatArgument<Operand>
-	{
-		template <typename Tuple, size_t... Indices>
-		FString Render(Tuple const& tuple, std::index_sequence<Indices...>&&)
-		{
-			auto body = Join(TEXT_", ", AsString(tuple.template Get<Indices>())...);
-			return TEXT_"(" + body + TEXT_")";
-		}
-		
-		template <CConvertibleToDecayed<Operand> Arg>
-		FString operator () (Arg&& left) const
-		{
-			return Render(left, std::make_index_sequence<TTupleArity<std::decay_t<Operand>>::Value>());
-		}
-	};
 
-	/** @brief Convert STL tuples to string the following way: `(Item0, Item1, Item2, ...)`*/
-	template <CStdTupleLike Operand>
+	/** @brief Convert Unreal/STL/Range-V3 tuples to string the following way: `(Item0, Item1, Item2, ...)`*/
+	template <CTuple Operand>
 	struct TAsFormatArgument<Operand>
 	{
 		template <typename Tuple, size_t... Indices>
 		FString Render(Tuple const& tuple, std::index_sequence<Indices...>&&)
 		{
-			auto body = Join(TEXT_", ", AsString(std::get<Indices>(tuple))...);
+			auto body = Join(TEXT_", ", AsString(GetItem<Indices>(tuple))...);
 			return TEXT_"(" + body + TEXT_")";
 		}
 		
 		template <CConvertibleToDecayed<Operand> Arg>
 		FString operator () (Arg&& left) const
 		{
-			return Render(left, std::make_index_sequence<std::tuple_size_v<std::decay_t<Operand>>>());
-		}
-	};
-
-	/** @brief Convert Range-V3 tuples to string the following way: `(Item0, Item1, Item2, ...)`*/
-	template <CRangeV3TupleLike Operand>
-	struct TAsFormatArgument<Operand>
-	{
-		template <typename Tuple, size_t... Indices>
-		FString Render(Tuple const& tuple, std::index_sequence<Indices...>&&)
-		{
-			auto body = Join(TEXT_", ", AsString(ranges::get<Indices>(tuple))...);
-			return TEXT_"(" + body + TEXT_")";
-		}
-		
-		template <CConvertibleToDecayed<Operand> Arg>
-		FString operator () (Arg&& left) const
-		{
-			return Render(left, std::make_index_sequence<std::tuple_size_v<std::decay_t<Operand>>>());
+			return Render(left, TIndexSequenceForTuple<Operand>());
 		}
 	};
 }
